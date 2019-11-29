0f9f608268639873d6fc24464fcbc7b7602c8e96
fazemahn <fazemahn@gmail.com>
2019-11-29 00:17:48 -0500

    final login, signup and logout system. Does not account for mismatch.

--- index.php
+++ index.php
@@ -1,13 +1,10 @@
 <?php
 require 'header.php' ?>
 <main>
-  <div class="leftPanel preLogin">
-    <h4>Signup</h4>
-    <form action="includes/signup.inc.php" method="post" name=signupSheet>
-      <input type="text" name="uname" placeholder="New Username">
-      <input type="password" name="passwd" placeholder="New Password">
-      <input type="submit" value="Sign Up">
-    </form>
-  </div>
+  <?php
+  if (!isset($_SESSION['userID'])){
+   require 'snippets/signupform.snip.php';
+ }
+  ?>
 </main>
 <?php require 'footer.php' ?>
